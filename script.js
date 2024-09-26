c888d0e1fe1c00b23254d27ff7c9816d0b1d9dc3
Kalsoom08 <kalsoomyousafzai20@gmail.com>
2024-09-27 00:54:13 +0500

    Main function is created

---
+++ script.js
@@ -0,0 +1,17 @@
+let input = document.getElementById("data-input");
+let button =  document.getElementById("add-button");
+let ul = document.getElementById("data-list");
+
+
+button.addEventListener('click', function(event) {
+  event.preventDefault(); 
+  var li = document.createElement('li');
+  li.textContent = input.value;
+  // ul.appendChild(li);
+  // input.value = "";
+  
+
+  // li.onclick = function () {
+  //   li.classList.toggle('completed');
+  // };
+});
